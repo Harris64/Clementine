dee399d652c1d848768f32353a33e5e39418b238
copsonm <copson1994@hotmail.com>
2014-02-24 13:26:42 +0000

    raw input

--- Client.py
+++ Client.py
@@ -4,10 +4,11 @@ SERVER_ADDRESS = "127.0.0.1"
 SERVER_PORT = 80
 
 size = 1024
+sendText= raw_input("please type your message: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER_ADDRESS, SERVER_PORT))
-client.send("Testing...")
+client.send(sendText)
 
 data = client.recv(size)
 
