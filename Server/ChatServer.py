e6faa6c476ae333ee4672fd43c0569be306d0b42
Harris Stevenson <harris64@outlook.com>
2014-02-26 12:49:42 +0000

    Update

--- Server/ChatServer.py
+++ Server/ChatServer.py
@@ -1,30 +1,53 @@
 import socket
 
+# ChatServer class.
 class ChatServer:
+
+    # Constructor method.
     def __init__(self, hostAddress, hostPort):
+
+        # Set object attributes.
         self.hostAddress = hostAddress
         self.hostPort = hostPort
-    
+
+    # Initialisation method.
     def initialise(self):
+
+        # Construct serverSocket, bind it and start listening.
         self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.serverSocket.bind((self.hostAddress, self.hostPort))
         self.serverSocket.listen(8)
 
+        # Set status flag.
         self.status = True
 
+    # Main loop method.
     def acceptConnections(self):
+
+        # Check status flag.
         while self.status == True:
+
+            # Receive connection and read data.
             connection, address = self.serverSocket.accept()
             data = connection.recv(1024)
 
+            # Check if data exists.
             if data != None:
+
+                # Send data back.
                 connection.send(data)
 
+            # Close connection.
             connection.close()
         else:
+
+            # Terminate server.
             self.terminate()
 
+    # Terminate method.
     def terminate(self):
+
+        # Close server socket.
         self.serverSocket.close()
 
 chatServer = ChatServer("127.0.0.1", 1234)
