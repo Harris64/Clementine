a38d2fb13428eb276093be498753e6db048e4e86
Harris Stevenson <harris64@outlook.com>
2014-03-18 09:31:57 +0000

    Update

--- Games/Harris/Game.py
+++ Games/Harris/Game.py
@@ -1,11 +1,29 @@
 import pyglet
 
-window = pyglet.window.Window()
-image = pyglet.resource.image("res/battlebacks/001-Grassland01.jpg")
+from pyglet.event import *
+from pyglet.gl import *
+from pyglet.graphics import *
+from pyglet.window import *
+
+try:
+	config = Config(sample_buffers = 1, samples = 4, depth_size = 16, double_buffer = True)
+	window = Window(config = config)
+except NoSuchConfigException:
+	window = Window()
+
+@window.event
+def on_resize(width, height):
+	glViewport(0, 0, width, height)
+	glMatrixMode(GL_PROJECTION)
+	glLoadIdentity()
+	glMatrixMode(GL_MODELVIEW)
+
+	return EVENT_HANDLED
 
 @window.event
 def on_draw():
-    window.clear()
-    image.blit(0, 0)
+	vertex_list.draw(GL_TRIANGLES)
+
+vertex_list = vertex_list(3, ('v2f', (0.0, 0.0, 1.0, 0.0, 1.0, 1.0)), ('c3B', (0, 0, 255, 0, 255, 0, 255, 0, 0)))
 
 pyglet.app.run()
